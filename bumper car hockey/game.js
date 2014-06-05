164b47d65f64549f1fe1679b0d70a64074dd05e7
hamme42 <jhammertime1@comcast.net>
2014-06-05 10:19:05 -0700

    Update game.js

--- bumper car hockey/game.js
+++ bumper car hockey/game.js
@@ -661,6 +661,7 @@ var ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload(car1_img_path);
 ASSET_MANAGER.queueDownload(car2_img_path);
 ASSET_MANAGER.queueDownload(puck_img_path);
+ASSET_MANAGER.queueDownload(dust_img_path);
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
